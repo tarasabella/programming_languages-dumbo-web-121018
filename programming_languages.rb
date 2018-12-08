ce461ed73cece3049b4d6afe47eec965c9c0186c
Tara Sabella <tarasabella@gmail.com>
2018-12-08 21:30:32 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,6 +1,27 @@
+# def reformat_languages(languages)
+#   new_languages = {}
+#   languages.each do |type, hash|
+#     hash.each do |name, attributes|
+#       new_languages[name] ||= attributes
+#       new_languages[name][:style] ||= []
+#       new_languages[name][:style] << type
+#     end
+#   end
+#   new_languages
+# end
+def num_points_scored(name)
+  game_hash.each do |location, team_data|
+    team_data[:players].each do |player_name, player_data|
+      if player_name == name 
+        #checks each name to ensure that it matches
+  return player_data[:points]
+      end
+    end
+  end
+end 
 def reformat_languages(languages)
   new_hash = {}
-  new_hash[:ruby] = {type: "interpreted",
+  languages.each do [type, data ] = {type: "interpreted",
     style: "oo"}
   },
   new_hash[:javascript] = {type: "interpreted",
