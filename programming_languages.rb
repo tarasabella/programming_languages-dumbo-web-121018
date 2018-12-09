8265b5658446014da4f23c2641f77814307fe1a6
Tara Sabella <tarasabella@gmail.com>
2018-12-09 21:42:33 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,11 +1,25 @@
+require "pry"
+# def reformat_languages(languages)
+#   new_languages = {}
+#   languages.each do |type, hash|
+#     hash.each do |name, attributes|
+#       new_languages[name] ||= attributes
+#       new_languages[name][:style] ||= []
+#       new_languages[name][:style] << type
+#     end
+#   end
+#   new_languages
+# end
 def reformat_languages(languages)
-  new_hash = {}
-  new_hash[:ruby] = {type: "interpreted",
-    style: "oo"}
-  },
-  new_hash[:javascript] = {type: "interpreted",
-    style: "oo", "functional"}
-  },
-  
-puts new_hash 
-end 
+  #define method to take in argument of languages hash 
+  attributes = {}
+  #create an empty hash to collect language attributes  
+  languages.each do |top_layer, language_hash|
+    #loop over the languages hash with each 
+    language_hash.each do |language, attribute_hash|
+      attributes[language] ||= attribute_hash
+      attributes[language][:style] ||= []
+      attributes[language][:style] << type 
+    end 
+  end 
+  attributes 
